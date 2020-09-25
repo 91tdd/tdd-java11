8c98a7d6e235cad5cd8145a04f99a851a002aedc
Joey Chen <jay.hatelove@gmail.com>
2020-09-25 16:41:19 +0800

    [test]add failed test "period_inside_budget_month"

--- src/test/java/BudgetService.java
+++ src/test/java/BudgetService.java
@@ -1,6 +1,13 @@
 import java.time.LocalDate;
 
 public class BudgetService {
+    private final IBudgetRepo repo;
+
+    public BudgetService(IBudgetRepo repo) {
+
+        this.repo = repo;
+    }
+
     public double totalAmount(LocalDate start, LocalDate end) {
         return 0;
     }
--- src/test/java/BudgetTests.java
+++ src/test/java/BudgetTests.java
@@ -1,21 +1,34 @@
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 public class BudgetTests {
 
-    private BudgetService budgetService = new BudgetService();
+    private IBudgetRepo repo = Mockito.mock(IBudgetRepo.class);
+    private BudgetService budgetService = new BudgetService(repo);
 
     @Test
     public void no_budgets() {
-        TotalAmountShouldBe(0
+        totalAmountShouldBe(0
                 , LocalDate.of(2020, 4, 1)
                 , LocalDate.of(2020, 4, 1));
     }
 
-    private void TotalAmountShouldBe(double expected, LocalDate start, LocalDate end) {
+    @Test
+    public void period_inside_budget_month() {
+        when(repo.getAll()).thenReturn(Arrays.asList(new Budget("202004", 30)));
+
+        totalAmountShouldBe(1
+                , LocalDate.of(2020, 4, 1)
+                , LocalDate.of(2020, 4, 1));
+    }
+
+    private void totalAmountShouldBe(double expected, LocalDate start, LocalDate end) {
         assertEquals(expected, budgetService.totalAmount(start, end), 0.00);
     }
 }
