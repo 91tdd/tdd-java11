72128cc81efd375c1b2f091a2189a71367e3f139
Joey Chen <jay.hatelove@gmail.com>
2020-09-25 17:27:13 +0800

    [refactor]change access level and rearrange code

--- src/test/java/Budget.java
+++ src/test/java/Budget.java
@@ -1,32 +1,15 @@
 import java.time.LocalDate;
-import java.time.Year;
 import java.time.YearMonth;
-import java.time.chrono.ChronoLocalDate;
-import java.time.format.DateTimeFormatter;
 
 import static java.time.format.DateTimeFormatter.ofPattern;
 
 public class Budget {
-    public final int amount;
+    private final int amount;
     private final String yearMonth;
 
     public Budget(String yearMonth, int amount) {
         this.yearMonth = yearMonth;
         this.amount = amount;
-    }
-
-    public LocalDate firstDay() {
-        return LocalDate.parse(yearMonth + "01", ofPattern("yyyyMMdd"));
-    }
-
-    public LocalDate lastDay() {
-        YearMonth month = getYearMonth();
-        return month.atEndOfMonth();
-    }
-
-    public int days() {
-        YearMonth month = getYearMonth();
-        return month.lengthOfMonth();
     }
 
     Period createPeriod() {
@@ -35,6 +18,20 @@ public class Budget {
 
     double getDailyAmount() {
         return (double) amount / days();
+    }
+
+    private LocalDate firstDay() {
+        return LocalDate.parse(yearMonth + "01", ofPattern("yyyyMMdd"));
+    }
+
+    private LocalDate lastDay() {
+        YearMonth month = getYearMonth();
+        return month.atEndOfMonth();
+    }
+
+    private int days() {
+        YearMonth month = getYearMonth();
+        return month.lengthOfMonth();
     }
 
     private YearMonth getYearMonth() {
