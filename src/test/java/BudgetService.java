917a5f838fd5c9fcfc361016ea365daceb5d5e3c
Joey Chen <jay.hatelove@gmail.com>
2020-09-25 16:33:17 +0800

    [test]add test "no budgets"

---
+++ src/test/java/BudgetService.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+
+public class BudgetService {
+    public double totalAmount(LocalDate start, LocalDate end) {
+        return 0;
+    }
+}
--- src/test/java/BudgetTests.java
+++ src/test/java/BudgetTests.java
@@ -1,4 +1,16 @@
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
 
 public class BudgetTests {
-
+    @Test
+    public void no_budgets() {
+        BudgetService budgetService = new BudgetService();
+        LocalDate start = LocalDate.of(2020, 4, 1);
+        LocalDate end = LocalDate.of(2020, 4, 1);
+        double totalAmount = budgetService.totalAmount(start, end);
+        assertEquals(0, totalAmount, 0.00);
+    }
 }
