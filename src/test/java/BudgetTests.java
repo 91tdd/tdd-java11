90e3488ef11dfe6dd97230eab422d1e48f461659
Joey Chen <jay.hatelove@gmail.com>
2020-09-25 17:05:51 +0800

    [test] add failed test: "period_overlap_budget_last_day"

--- src/test/java/BudgetTests.java
+++ src/test/java/BudgetTests.java
@@ -56,6 +56,15 @@ public class BudgetTests {
                 , LocalDate.of(2020, 4, 1));
     }
 
+    @Test
+    public void period_overlap_budget_last_day() {
+        givenBudgets(new Budget("202004", 30));
+
+        totalAmountShouldBe(1
+                , LocalDate.of(2020, 4, 30)
+                , LocalDate.of(2020, 5, 1));
+    }
+
     private void givenBudgets(Budget... budgets) {
         when(repo.getAll()).thenReturn(Arrays.asList(budgets));
     }
