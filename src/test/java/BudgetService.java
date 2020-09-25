d41aa042b8abc577c334b0547515186f3c2cd5ab
Joey Chen <jay.hatelove@gmail.com>
2020-09-25 17:06:30 +0800

    [feat]implement overlappingEnd

--- src/test/java/BudgetService.java
+++ src/test/java/BudgetService.java
@@ -20,7 +20,10 @@ public class BudgetService {
             LocalDate overlappingStart = start.isAfter(budget.firstDay())
                     ? start
                     : budget.firstDay();
-            return DAYS.between(overlappingStart, end) + 1;
+            LocalDate overlappingEnd = end.isBefore(budget.lastDay())
+                    ? end
+                    : budget.lastDay();
+            return DAYS.between(overlappingStart, overlappingEnd) + 1;
         }
         return 0;
     }
