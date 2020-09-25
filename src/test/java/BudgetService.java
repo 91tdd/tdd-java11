6501ba31805507cfd85859fb1f95df346ad01520
Joey Chen <jay.hatelove@gmail.com>
2020-09-25 16:43:27 +0800

    fake implement: hard-code return 1 when any budget exists

--- src/test/java/BudgetService.java
+++ src/test/java/BudgetService.java
@@ -1,14 +1,18 @@
 import java.time.LocalDate;
+import java.util.List;
 
 public class BudgetService {
     private final IBudgetRepo repo;
 
     public BudgetService(IBudgetRepo repo) {
-
         this.repo = repo;
     }
 
     public double totalAmount(LocalDate start, LocalDate end) {
+        List<Budget> budgets = repo.getAll();
+        if (budgets.size() > 0) {
+            return 1;
+        }
         return 0;
     }
 }
