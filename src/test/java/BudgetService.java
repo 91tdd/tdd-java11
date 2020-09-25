809b94644de841d774880394deddeda244078db2
Joey Chen <jay.hatelove@gmail.com>
2020-09-25 17:06:56 +0800

    [refactor]extract method overlappingDays()

--- src/test/java/BudgetService.java
+++ src/test/java/BudgetService.java
@@ -14,17 +14,21 @@ public class BudgetService {
         List<Budget> budgets = repo.getAll();
         if (budgets.size() > 0) {
             Budget budget = budgets.get(0);
-            if (end.isBefore(budget.firstDay()) || start.isAfter(budget.lastDay())) {
-                return 0;
-            }
-            LocalDate overlappingStart = start.isAfter(budget.firstDay())
-                    ? start
-                    : budget.firstDay();
-            LocalDate overlappingEnd = end.isBefore(budget.lastDay())
-                    ? end
-                    : budget.lastDay();
-            return DAYS.between(overlappingStart, overlappingEnd) + 1;
+            return overlappingDays(start, end, budget);
         }
         return 0;
+    }
+
+    private long overlappingDays(LocalDate start, LocalDate end, Budget budget) {
+        if (end.isBefore(budget.firstDay()) || start.isAfter(budget.lastDay())) {
+            return 0;
+        }
+        LocalDate overlappingStart = start.isAfter(budget.firstDay())
+                ? start
+                : budget.firstDay();
+        LocalDate overlappingEnd = end.isBefore(budget.lastDay())
+                ? end
+                : budget.lastDay();
+        return DAYS.between(overlappingStart, overlappingEnd) + 1;
     }
 }
