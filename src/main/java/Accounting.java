5b8be17b550f81d7272763ccaf69d0fd1f1610ca
Joey Chen <jay.hatelove@gmail.com>
2021-02-12 18:32:46 +0800

    refactor: extract method dailyAmount()

--- src/main/java/Accounting.java
+++ src/main/java/Accounting.java
@@ -14,9 +14,13 @@ public class Accounting {
         if (budgets.size() > 0) {
             Budget budget = budgets.get(0);
             Period period = new Period(start, end);
-            double dailyAmount = budget.amount / budget.days();
+            double dailyAmount = dailyAmount(budget);
             return dailyAmount * period.overlappingDays(budget.createPeriod());
         }
         return 0;
+    }
+
+    private double dailyAmount(Budget budget) {
+        return budget.amount / budget.days();
     }
 }
