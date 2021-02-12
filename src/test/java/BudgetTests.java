d47956dd7b4328bcd8f0477d94151712087dfc2d
Joey Chen <jay.hatelove@gmail.com>
2021-02-12 18:00:23 +0800

    refactor test

--- src/test/java/BudgetTests.java
+++ src/test/java/BudgetTests.java
@@ -5,12 +5,19 @@ import java.time.LocalDate;
 import static org.junit.Assert.assertEquals;
 
 public class BudgetTests {
+
+    private final Accounting accounting = new Accounting();
+
     @Test
     public void no_budgets() {
+        totalAmountShouldBe(0,
+                LocalDate.of(2000, 4, 1),
+                LocalDate.of(2000, 4, 1));
+    }
 
-        Accounting accounting = new Accounting();
-        assertEquals(0,
-                accounting.totalAmount(LocalDate.of(2000, 4, 1), LocalDate.of(2000, 4, 1)),
+    private void totalAmountShouldBe(int expected, LocalDate start, LocalDate end) {
+        assertEquals(expected,
+                accounting.totalAmount(start, end),
                 0.00);
     }
 }
