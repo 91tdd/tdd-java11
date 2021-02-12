dcea8873fd03cefaae0fde31366b72dc14b899c3
Joey Chen <jay.hatelove@gmail.com>
2021-02-12 18:05:50 +0800

    feat: interval days of period

--- src/main/java/Accounting.java
+++ src/main/java/Accounting.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 import java.util.List;
 
+import static java.time.temporal.ChronoUnit.DAYS;
+
 public class Accounting {
     private final IBudgetRepo budgetRepo;
 
@@ -12,7 +14,8 @@ public class Accounting {
     public double totalAmount(LocalDate start, LocalDate end) {
         List<Budget> budgets = budgetRepo.getAll();
         if (budgets.size() > 0) {
-            return 1;
+            long days = DAYS.between(start, end) + 1;
+            return days;
         }
         return 0;
     }
