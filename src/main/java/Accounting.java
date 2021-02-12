9aa2c7e68906aaf6d828cea26029d7d6a1f473f1
Joey Chen <jay.hatelove@gmail.com>
2021-02-12 17:59:36 +0800

    test: add test, no_budgets

---
+++ src/main/java/Accounting.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+
+public class Accounting {
+    public double totalAmount(LocalDate start, LocalDate end) {
+        return 0;
+    }
+}
--- src/test/java/BudgetTests.java
+++ src/test/java/BudgetTests.java
@@ -1,4 +1,16 @@
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
 
 public class BudgetTests {
+    @Test
+    public void no_budgets() {
 
+        Accounting accounting = new Accounting();
+        assertEquals(0,
+                accounting.totalAmount(LocalDate.of(2000, 4, 1), LocalDate.of(2000, 4, 1)),
+                0.00);
+    }
 }
